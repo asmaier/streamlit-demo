4e0b8b11dd56d4fb0a5642af3ddbea48c9cfde6d
Andreas Maier <andreas.maier@unbelievable-machine.com>
2020-10-20 10:05:44 +0200

    Added Pipfile. Added streamlit_test from streamlit tutorial. Extended dachs with many more plots.

--- dachs.py
+++ dachs.py
@@ -1,8 +1,55 @@
 import streamlit as st
 import pandas_datareader.data as web
+import seaborn as sns
+import io
 
 f = web.DataReader('^DAX', 'stooq')
 
 f
 
+dax = f[["Close"]]
 
+col1, col2 = st.beta_columns(2)
+
+with col1:
+    # it is not yet possible to center the content
+    st.write(dax.describe())
+
+with col2:
+    # By default info() does only return None
+    # So we need some workaround to display the output
+    # see https://stackoverflow.com/questions/39440253/how-to-return-a-string-from-pandas-dataframe-info
+    buf = io.StringIO()
+    dax.info(buf=buf, verbose=True)
+    st.text(buf.getvalue())
+
+
+st.line_chart(dax)
+
+# How to work with pandas inbuilt plotting.
+# It needs optional pandas dependency matplotlib
+# One has to select thr property ".figure" from the pandas
+# output
+st.pyplot(dax.plot().figure)
+
+# hist returns a numpy array of subplots
+# we need to select the first one
+hist = dax.hist(bins=100)
+st.pyplot(hist[0, 0].figure)
+
+# we can also use seaborne
+# set_theme will globally change the layout of matplotlib plots
+sns.set_theme()
+
+st.pyplot(sns.relplot(data=dax, kind="line"))
+
+
+st.pyplot(sns.pairplot(data=f[["Close","Volume"]], markers="+", kind="reg", diag_kind="kde"))
+
+# dow
+f = web.DataReader('^DJI', 'stooq')
+dow = f[["Close"]]
+
+new_data = dax.merge(dow, left_index=True, right_index=True, suffixes=('_dax', '_dow'))
+
+st.pyplot(sns.pairplot(data=new_data, markers="+", kind="reg", diag_kind="kde"))
---
+++ streamlit_test.py
@@ -0,0 +1,116 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import time
+
+st.title('My first app')
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+df
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+
+option = st.selectbox(
+    'Which number do you like best?',
+    df['first column'])
+
+'You selected: ', option
+
+option = st.sidebar.selectbox(
+    'Which number do you like best?',
+    df['first column'], key=123)
+
+'You selected:', option
+
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+    right_column.write("Woohoo!")
+
+expander = st.beta_expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
+
+progress_bar = st.progress(0)
+status_text = st.empty()
+chart = st.line_chart(np.random.randn(10, 2))
+
+for i in range(100):
+    # Update progress bar.
+    progress_bar.progress(i + 1)
+
+    new_rows = np.random.randn(10, 2)
+
+    # Update status text.
+    status_text.text(
+        'The latest random number is: %s' % new_rows[-1, 1])
+
+    # Append data to the chart.
+    chart.add_rows(new_rows)
+
+    # Pretend we're doing some computation that takes time.
+    time.sleep(0.1)
+
+status_text.text('Done!')
+st.balloons()
+
+st.text('This will appear first')
+# Appends some text to the app.
+
+my_slot1 = st.empty()
+# Appends an empty slot to the app. We'll use this later.
+
+my_slot2 = st.empty()
+# Appends another empty slot.
+
+st.text('This will appear last')
+# Appends some more text to the app.
+
+my_slot1.text('This will appear second')
+# Replaces the first empty slot with a text string.
+
+my_slot2.line_chart(np.random.randn(20, 2))
+# Replaces the second empty slot with a chart.
+
+# Get some data.
+data = np.random.randn(10, 2)
+
+# Show the data as a chart.
+chart = st.line_chart(data)
+
+# Wait 1 second, so the change is clearer.
+time.sleep(1)
+
+# Grab some more data.
+data2 = np.random.randn(10, 2)
+
+# Append the new data to the existing chart.
+chart.add_rows(data2)
+
